06ab9074f5c599a033c345772cfaa8c0bb4061d1
Gib-ON Design <gibonwroc@gmail.com>
2023-03-27 17:27:33 +0200

    working code v1.0

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
+const plusBtn = document.querySelector('.sizeUp')
+const minusBtn = document.querySelector('.sizeDown')
+const colorBtn = document.querySelector('.color')
+const p1 = document.querySelector('.text p ')
+const styles = window.getComputedStyle(p1);
+const paragraphFontSize = styles.getPropertyValue('font-size'); 
+let basicFontSize = parseInt(paragraphFontSize)
+
+
+const fontIncrease = () => {
+    basicFontSize += 5
+    p1.style.fontSize = basicFontSize + 'px'
+}
+
+const fontDecrease = () => {
+    basicFontSize -= 5
+    p1.style.fontSize = basicFontSize + 'px'
+}
 
 
 
-const p = document.querySelector('p')
+const colorChange = () => {
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    p1.style.color = `rgb(${r},${g},${b})`
+}
 
-console.log(p.hasAttribute('style'));
 
 
-p.removeAttribute('nazwa_atrybutu')
-// usuwa wskazany atrybut
+
+plusBtn.addEventListener('click', fontIncrease)
+minusBtn.addEventListener('click', fontDecrease)
+colorBtn.addEventListener('click', colorChange)
