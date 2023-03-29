31243c9083f21b75143d83a4cf7f97988646d680
Gib-ON Design <gibonwroc@gmail.com>
2023-03-29 09:13:49 +0200

    final code

--- js/main.js
+++ js/main.js
@@ -1,51 +1,34 @@
-const plusBtn = document.querySelector('.sizeUp')
-const minusBtn = document.querySelector('.sizeDown')
-const colorBtn = document.querySelector('.color')
-const p1 = document.querySelector('.text p ')
-const resetBtn = document.querySelector('.reset')
+const plusBtn = document.querySelector(".sizeUp");
+const minusBtn = document.querySelector(".sizeDown");
+const colorBtn = document.querySelector(".color");
+const p1 = document.querySelector(".text p ");
+const resetBtn = document.querySelector(".reset");
 const styles = window.getComputedStyle(p1);
-const paragraphFontSize = styles.getPropertyValue('font-size'); 
-let basicFontSize = parseInt(paragraphFontSize)
-
-
+const paragraphFontSize = styles.getPropertyValue("font-size");
+let basicFontSize = parseInt(paragraphFontSize);
 
 const fontIncrease = () => {
-   if (basicFontSize <=60) {
-     basicFontSize += 5
-     p1.style.fontSize = basicFontSize + 'px'
-   }
-}
+	if (basicFontSize <= 51) basicFontSize += 5;
+	p1.style.fontSize = basicFontSize + "px";
+};
 
 const fontDecrease = () => {
-
-
-
-    if (basicFontSize >=16 ) {
-        
-        basicFontSize -= 5
-        p1.style.fontSize = basicFontSize + 'px'
-        console.log(parseInt(p1.style.fontSize));
-    }
-    
-}
-
-
+	if (basicFontSize >= 16) basicFontSize -= 5;
+	p1.style.fontSize = basicFontSize + "px";
+};
 
 const colorChange = () => {
-    let r = Math.floor(Math.random() * 256)
-    let g = Math.floor(Math.random() * 256)
-    let b = Math.floor(Math.random() * 256)
-    p1.style.color = `rgb(${r},${g},${b})`
-}
-
-
+	let r = Math.floor(Math.random() * 256);
+	let g = Math.floor(Math.random() * 256);
+	let b = Math.floor(Math.random() * 256);
+	p1.style.color = `rgb(${r},${g},${b})`;
+};
 
 const resetStyle = () => {
-    p1.removeAttribute('style')
-}
+	p1.removeAttribute("style");
+};
 
-plusBtn.addEventListener('click', fontIncrease)
-minusBtn.addEventListener('click', fontDecrease)
-colorBtn.addEventListener('click', colorChange)
-resetBtn.addEventListener('click', resetStyle)
-
+plusBtn.addEventListener("click", fontIncrease);
+minusBtn.addEventListener("click", fontDecrease);
+colorBtn.addEventListener("click", colorChange);
+resetBtn.addEventListener("click", resetStyle);
